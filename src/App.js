01717658135cfeaf9ff23d21a7ae9726e1f8401d
Debnath <souvik.debnath@refinitiv.com>
2021-06-06 08:27:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React from "react";
 import logo from './logo.svg';
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Container fluid>
+        <Todo />
+        <TodoForm />
+      </Container>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+import {
+    Container,
+    FormGroup,
+    Input,
+    Button,
+    Form,
+    InputGroup,
+    InputGroupAddon
+} from "reactstrap";
+
+import { v4 } from "uuid";
+
+import { connect } from "react-redux";
+import { addTodo } from "../action/todo";
+
+const TodoForm = ({ addTodo }) => {
+
+    const [title, setTitle] = useState('');
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (title === "") {
+            return alert("please add a todo")
+        }
+        const todo = {
+            title,
+            id: v4()
+        }
+
+        addTodo(todo);
+        setTitle("");
+    };
+    
+    
+    
+    return (
+        <Form onSubmit={handleSubmit}>
+            <FormGroup>
+                <Input
+                    type="text"
+                    name="todo"
+                    id="todo"
+                    placeholder="Your next Todo"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <InputGroupAddon addonType="prepend">
+                    <Button color="primary" onClick={handleSubmit}>ADD</Button>
+                </InputGroupAddon>
+            </FormGroup>
+        </Form>
+    );
+};
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({
+    addTodo: todo => {
+        dispatch(addTodo(todo));
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
